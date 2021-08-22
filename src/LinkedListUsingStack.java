d556a340c73e85bdc2cd2f7aa43871b2f0f23e18
Anurodh437 <anurodhdubey437@gmail.com>
2021-08-22 17:48:21 +0530

    added new files

--- src/LinkedListUsingStack.java
+++ src/LinkedListUsingStack.java
@@ -1,44 +1,52 @@
-import java.util.LinkedList;
 import java.util.Stack;
 
-public class LinkedListUsingStack {
+class LinkedListUsingStack {
     static class Node {
         int data;
         Node next;
+    }
+    static Node head = null;
 
-        static Node head = null;
+    static void push(int newData) {
+        Node newNode = new Node();
 
-        static void push(int newData) {
-            Node newNode = new Node();
+        newNode.data = newData;
+        newNode.next = (head);
+        (head) = newNode;
+    }
 
-            newNode.data = newData;
-            newNode.next = head;
-            head = newNode;
+    static Node reverseList(Node head) {
+        Stack<Node> stack = new Stack<>();
+
+        Node current = head;
+        while (current.next != null)
+        {
+            stack.push(current);
+            current = current.next;
         }
-
-        static Node reverseList(Node head) {
-            Stack<Node> stack = new Stack<>();
-            Node current = head;
-
-            while (current.next != null) {
-                stack.push(current);
-                current = current.next;
-            }
-            head = current;
-            while (!stack.isEmpty()) {
-                current.next = stack.peek();
-                current = current.next;
-            }
-            return head;
+        head = current;
+        while (!stack.isEmpty()) {
+            current.next = stack.peek();
+            current = current.next;
+        }
+        current.next = null;
+        return head;
+    }
+    static void printList(Node head)
+    {
+        while (head != null)
+        {
+            System.out.println(head.data + " ");
+            head = head.next;
         }
     }
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        System.out.println(list);
+        push(1);
+        push(2);
+        push(3);
+        push(4);
+        head = reverseList(head);
+        printList(head);
     }
 }
